ce3f0587d2a16e3c07e1c85cceecab20b51ed5b3
Thomas Prange <tjprange@gmail.com>
2021-03-25 12:47:58 -0700

    add comments

--- models/user.js
+++ models/user.js
@@ -13,14 +13,18 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+// add function to userSchema: find user and validate w/ password
 userSchema.statics.findAndValidate = async function (username, password) {
   const foundUser = await this.findOne({ username });
   const isValid = await bcrypt.compare(password, foundUser.password);
   return isValid ? foundUser : false;
 };
 
+// "this" refers to instance of User
+// function is called before user.save() in /register route
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
+  // hash the pw
   this.password = await bcrypt.hash(this.password, 12);
   next();
 });
